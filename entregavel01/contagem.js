6be8626c4ece53113dcadb5a8333b6d6ed7a0a2c
Luis Gustavo <luisgustavobl10@gmail.com>
2026-05-19 18:53:23 -0300

    Arquivos JS e JAVA do entregável 1

---
+++ entregavel01/contagem.js
@@ -0,0 +1,9 @@
+function contagem(n, dados) {
+    let contador = 0;
+    for (let i = 0; i < dados.length; i++) {
+        if (Number.isInteger(dados[i]) && dados[i] <= n) {
+            contador++;
+        }
+    }
+    return contador;
+}
